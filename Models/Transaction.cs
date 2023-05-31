a05d7ddbe6ba2e4172deb604abc569d75a22e105
Jeethendra S R <jeethendra@kerv.com>
2023-05-31 19:52:51 +0530

    Initial Commit

---
+++ Models/Transaction.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions_CRUD_.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required]
+        [MaxLength(12, ErrorMessage = "Maximum of 12 characters")]
+        public string? AccountNumber { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required]
+        public string? BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required]
+        public string? BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("IFSC Code")]
+        [Required(ErrorMessage = "IFSC is Must")]
+        [MaxLength(11, ErrorMessage = "Maximum of 11 characters")]
+        public string? IFSCCode { get; set; }
+
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions_CRUD_.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
